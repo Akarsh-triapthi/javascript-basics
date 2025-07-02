33be0ade04677f0a2b6b05b9606d1970f63ca9b0
Akarsh-triapthi <110883110+Akarsh-triapthi@users.noreply.github.com>
2025-07-02 18:17:27 +0000

    data

---
+++ 01/data.js
@@ -0,0 +1,14 @@
+const AccountName = "Ram"
+let AccountId = 112
+let AccountStatus = true
+let AccountDeposit = null
+ console.log(typeof 112, typeof true, typeof "ram")
+ //data type
+ // number = 112
+ // bigint = big values
+ //boolean = true/false
+ //string = "Ram"
+ // null = standalone value (its type is object)
+ // undefined 
+ // symbol = unuique
+ //object
